ae204de41bdb1626ea2c4b6e6a46ede3dd100acd
Will Hedgecock <will.hedgecock@gmail.com>
2026-05-16 16:11:43 -0500

    Change MQTT alert QOS default to "0"

--- Common/Inc/common.h
+++ Common/Inc/common.h
@@ -92,14 +92,14 @@
   #define MAX(a, b)                          (((a) > (b)) ? (a) : (b))
 #endif
 
-#define DEFAULT_CONFIG_INITIALIZATION_TAG                         98
+#define DEFAULT_CONFIG_INITIALIZATION_TAG                         99
 #define DEFAULT_DEVICE_STATUS_UPDATE_INTERVAL_MINUTES             15
 #define DEFAULT_SD_STORAGE_AUDIO_CLIP_MIN_SECONDS                 3
 #define DEFAULT_SD_STORAGE_PROBABILITY_THRESHOLD                  -0.5f
 #define DEFAULT_MIN_SHOT_ALERT_PROBABILITY                        -0.5f
 #define DEFAULT_GOOD_SHOT_ALERT_PROBABILITY                       0.0f
 #define DEFAULT_MQTT_DEVICE_INFO_QOS                              0
-#define DEFAULT_MQTT_ALERT_QOS                                    1
+#define DEFAULT_MQTT_ALERT_QOS                                    0
 #define DEFAULT_MQTT_AUDIO_CLIP_QOS                               0
 
 #define DMA_STREAM0_4_INDEX                  0U
